fd403f8296ead06769fe525e401f6fa82789f242
CamiloCedenoC <154028859+CamiloCedenoC@users.noreply.github.com>
2024-01-29 00:10:36 -0500

    Add files via upload

---
+++ src/main/java/com/unir/eurekagrupo13/EurekaGrupo13Application.java
@@ -0,0 +1,13 @@
+package com.unir.eurekagrupo13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaGrupo13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaGrupo13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/eurekagrupo13/EurekaGrupo13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.eurekagrupo13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaGrupo13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
