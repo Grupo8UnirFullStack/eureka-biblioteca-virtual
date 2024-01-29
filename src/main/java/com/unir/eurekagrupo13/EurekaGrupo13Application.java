e6bff102ccdec79ee7bf253113f0ab8cc4c5baae
Bryan Cedeno <bcedeno@territoriosinteligentes.net>
2024-01-29 00:25:41 -0500

    ADD Anotación eureka server

--- src/main/java/com/unir/eurekagrupo13/EurekaGrupo13Application.java
+++ src/main/java/com/unir/eurekagrupo13/EurekaGrupo13Application.java
@@ -2,8 +2,10 @@ package com.unir.eurekagrupo13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaGrupo13Application {
 
 	public static void main(String[] args) {
